7936f3f41abc0f5064f44a16552e6c531c83c00f
Jack Ryan <jack.ryan@intercom.io>
2021-02-05 15:36:46 +0000

    Yabai

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
-sudo rm -rf ~/.vim > /dev/null 2>&1
-sudo rm -rf ~/.vimrc > /dev/null 2>&1
-sudo rm -rf ~/.tmux > /dev/null 2>&1
-sudo rm -rf ~/.zshrc > /dev/null 2>&1
-sudo rm -rf ~/.config > /dev/null 3>&1
+#sudo rm -rf ~/.vim > /dev/null 2>&1
+#sudo rm -rf ~/.vimrc > /dev/null 2>&1
+#sudo rm -rf ~/.tmux > /dev/null 2>&1
+#sudo rm -rf ~/.zshrc > /dev/null 2>&1
+#sudo rm -rf ~/.config > /dev/null 3>&1
 
 
 ln -sf ~/dotfiles/vimrc ~/.vimrc
